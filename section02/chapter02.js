f4add95d399174b2929b23c3a95674282b7118ea
suding <jhw02045@naver.com>
2024-11-25 22:37:21 +0900

    단락평가 사용법

---
+++ section02/chapter02.js
@@ -0,0 +1,31 @@
+// function returnFalse() {
+//     console.log("False 함수");
+//     return undefined;
+// }
+
+// function returnTrue() {
+//     console.log("True 함수");
+//     return 10;
+// }
+
+//단락평가 작동 X
+// console.log(returnTrue() && returnFalse());
+
+//단락평가 작동 O
+// console.log(returnFalse() && returnTrue());
+
+//단락평가 작동 O
+// console.log(returnTrue() || returnFalse());
+
+// console.log(returnFalse() && returnTrue());
+
+// 단락 평가 활용 사례
+function printName(person) {
+    const name = person && person.name;
+    console.log(name || "person의 값이 없음");
+}
+
+printName();
+printName({
+    name: "이정환",
+});
