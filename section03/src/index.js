63256ab6afd3e29ad107c3e86eacb3b7f437285f
sinsujung <jhw02045@naver.com>
2024-12-04 16:01:31 +0900

    라이브러리 import 하는 법

--- section03/src/index.js
+++ section03/src/index.js
@@ -8,9 +8,14 @@
 //esm
 import mul, {add, sub} from "./math.js";
 
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
 //dafault 함수 가져올 때
 // import mul from "./math.js";
-console.log(add(1,2));
-console.log(sub(1,2));
-console.log(mul(2,3));
+// console.log(add(1,2));
+// console.log(sub(1,2));
+// console.log(mul(2,3));
+
 
