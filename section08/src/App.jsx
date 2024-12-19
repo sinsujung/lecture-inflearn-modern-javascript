0619f8a8698af653d84134c3e03ea7668bc07abb
sinsujung <jhw02045@naver.com>
2024-12-19 18:22:27 +0900

    TodoList 앱 UI 구현

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import Header from "./components/Header";
+import Editor from "./components/Editor";
+import List from "./components/List";
 
 function App() {
 
   return (
-    <>
-     TodoList
-    </>
+    <div className="App">
+     <Header />
+     <Editor />
+     <List />
+    </div>
   )
 }
 
