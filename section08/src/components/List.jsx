77b69389d2503f675c1887bf4b11f1e9e11fb793
sinsujung <jhw02045@naver.com>
2024-12-21 18:00:54 +0900

    투두리스트 랜더링하기

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,15 +1,31 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
-const List = () => {
+import {useState} from "react";
+const List = ({todos}) => {
+    const [search, setSearch] = useState("");
+
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    };
+
+    const getFilteredDate = () => {
+        if(search === "") {
+            return todos;
+        }
+        return todos.filter((todo) => 
+            todo.content.toLowerCase().includes(search.toLowerCase()));
+    };
+
+    const filteredTodos = getFilteredDate();
     return (
         <div className="List">
             <h4>Todo List 🌱</h4>
-            <input placeholder="검색어를 입력하세요"></input>
+            <input value={search} onChange={onChangeSearch} placeholder="검색어를 입력하세요"></input>
 
             <div className="todos_wrapper">
-                <TodoItem/>
-                <TodoItem/>
-                <TodoItem/>
+                {filteredTodos.map((todo)=> {
+                    return <TodoItem key={todo.id} {...todo}/>
+                })}
             </div>
         </div>
     );
