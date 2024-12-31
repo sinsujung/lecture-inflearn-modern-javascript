eb33ac750733aab2c4dadd3ba1fad002563c72b0
sinsujung <jhw02045@naver.com>
2025-01-01 02:57:30 +0900

    New 페이지 기능 구현

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -2,9 +2,21 @@ import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const New = () => {
+    const { onCreate } = useContext(DiaryDispatchContext);
+
     const nav = useNavigate();
+
+    const onSubmit = (input) => {
+            onCreate(input.createdDate.getTime(), 
+            input.emotionId, 
+            input.content
+        );
+        nav('/', {replace: true});
+    };
 
     return (
         <div>
@@ -13,6 +25,9 @@ const New = () => {
                 <Button onClick={() => nav(-1)} text={"< 뒤로 가기"}/>
             }
             />
+            <Editor onSubmit={onSubmit}/>
         </div>
     );
 }
+
+export default New;
