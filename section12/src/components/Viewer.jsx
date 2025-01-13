9214d1fded1ee6cd74845ee882e2c9c5a8c09196
sinsujung <jhw02045@naver.com>
2025-01-13 17:45:25 +0900

    diary 페이지 기능 구현

--- section12/src/components/Viewer.jsx
+++ section12/src/components/Viewer.jsx
@@ -2,9 +2,8 @@ import "./Viewer.css";
 import { getEmotionImage } from "../util/get-emotion-image";
 import { emotionList } from "../util/constants";
 
-const Viewer = () => {
-    const emotionId = 1;
-    
+const Viewer = ({emotionId, content}) => {
+
     const emotionItem = emotionList.find(
         (item) => String(item.emotionId) === String(emotionId)
     );
@@ -23,7 +22,7 @@ const Viewer = () => {
             <section className="content_section">
                 <h4>오늘의 일기</h4>
                 <div className="content_wrapper">
-                    <p>일기 ....</p>
+                    <p>{content}</p>
                 </div>
             </section>
         </div>
